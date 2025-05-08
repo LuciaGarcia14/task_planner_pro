34c2584873b0ec178627029a261bf65a3f597750
LuciaGarcia14 <luciiagmon@gmail.com>
2025-05-08 21:25:08 +0200

    6 commit. Creamos nuestros ficheros de app.js (principal), en la carpeta routes los routes para los usarios y por último creamos la carpeta middleware para la utenticación del token.

---
+++ Backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,25 @@
+const { json } = require('body-parser');
+const jwt = require('jsonwebtoken');
+const secret_key = '1652013pP';
+
+const verify_token = (allowedPermission = []) => (req, res, next) => {
+    let token = req.cookies.token;
+
+    if(!token) return res.status(401).json({ message: 'No token provided' });
+
+    try{
+        const decoded = jwt.verify(token, secret_key);
+        req.user = decoded;
+
+        if(allowedPermission.length > 0 && (!req.user || !allowedPermission.includes(req.user.role))){
+            return res.status(403).json({ message: 'You do not have permission'});
+
+            next();
+        }
+
+    }catch(error){
+        return res.status(401).json({ message: 'Invalid Token '});
+    }
+}
+
+module.exports = { verify_token };
--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+const { verifyToken } = require('../middlewares/authMiddleware')
+
+router.post('/login', userController.loginUser);
+router.get('/verify_token', userController.verifyToken);
+
+router.get('/', verifyToken(['admin']), userController.getUserController);
+router.get('/:id', verifyToken(['admin']), userController.getUserById);
+router.post('/', verifyToken(['admin']), userController.createUser);
+router.delete('/:id', verifyToken(['admin']), userController.deleteUser);
+router.put('/:id', verifyToken(['admin']), userController.updateUser);
+
+module.exports = router;
 
